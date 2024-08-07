f540cf12f31aa6df37a25ab6473635a5ee4d2c86
Rohit3edge <rohit@3edgetechnologies.com>
2024-08-07 21:57:46 +0530

    New

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './component/Home';
-// import Ledger from './component/Ledger.jsx';
+import Ledger from './component/Ledger.jsx';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 //auth
 import Login from './component/Login';
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<Home />} />       
         <Route exact path="/login" element={<Login />} />
-        {/* <Route exact path="/Ledger" element={<Ledger />} /> */}
+        <Route exact path="/Ledger" element={<Ledger />} />
         <Route path='*' element = {<Notfoundpage/>} />
       </Routes>
     </Router>
